76ae5441bfdce2d04ec70d54952dd8705e153b4d
Kevin Chen <kevinchen8223@gmail.com>
2013-03-10 01:02:58 -0800

    Created PracticeBook to learn Rails/Git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Practicebook::Application.config.secret_token = '93481fa383bab219f2e523f15f9be1b9292d423418396b4b90cf91350b68cdd7f1f860678822ae0cb47d5c6fa9c87ad4847b188ebc445d674fa8443299b41f98'
